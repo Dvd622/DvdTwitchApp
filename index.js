6b24e097fdac50f1da61211e3d618bbee6b8acfd
Dvd <44392695+Dvd622@users.noreply.github.com>
2023-06-05 23:28:46 +1000

    Initial Commit
    Add index.js
    Add serverController.js
    Add package.json
    Add package-lock.json

---
+++ index.js
@@ -0,0 +1,150 @@
+// Define our dependencies
+const express        = require('express');
+const session        = require('express-session');
+const passport       = require('passport');
+const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+const request        = require('request');
+const handlebars     = require('handlebars');
+const axios          = require('axios');
+const controller     = require('./serverController');
+require('dotenv').config();
+
+// Define our constants, you will change these with your own
+const TWITCH_CLIENT_ID = process.env.CLIENT_ID;
+const TWITCH_SECRET    = process.env.SECRET_KEY;
+const SESSION_SECRET   = 'test123';
+const CALLBACK_URL     = 'http://localhost:3000/auth/twitch/callback';  // You can run locally with - http://localhost:3000/auth/twitch/callback
+
+// Initialize Express and middlewares
+var app = express();
+app.use(session({secret: SESSION_SECRET, resave: false, saveUninitialized: false}));
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Override passport profile function to get user profile from Twitch API
+OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
+  var options = {
+    url: 'https://api.twitch.tv/helix/users',
+    method: 'GET',
+    headers: {
+      'Client-ID': TWITCH_CLIENT_ID,
+      'Accept': 'application/vnd.twitchtv.v5+json',
+      'Authorization': 'Bearer ' + accessToken
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (response && response.statusCode == 200) {
+      done(null, JSON.parse(body));
+    } else {
+      done(JSON.parse(body));
+    }
+  });
+}
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use('twitch', new OAuth2Strategy({
+    authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+    tokenURL: 'https://id.twitch.tv/oauth2/token',
+    clientID: TWITCH_CLIENT_ID,
+    clientSecret: TWITCH_SECRET,
+    callbackURL: CALLBACK_URL,
+    state: true,
+  },
+  function(accessToken, refreshToken, profile, done) {
+    profile.accessToken = accessToken;
+    profile.refreshToken = refreshToken;
+    //profile.display_name = profile.display_name;
+    //console.log(profile.data[0]);
+    profile.id = profile.data[0].id;
+    profile.display_name = profile.data[0].display_name;
+    profile.bio = profile.data[0].description;
+    profile.image_url = profile.data[0].profile_image_url;
+    profile.test = "test";
+
+    // Securely store user profile in your DB
+    //User.findOrCreate(..., function(err, user) {
+    //  done(err, user);
+    //});
+
+    done(null, profile);
+  }
+));
+
+// Set route to start OAuth link, this is where you define scopes to request
+app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'user:read:email analytics:read:games channel:read:subscriptions bits:read channel:manage:polls channel:manage:predictions' }));
+
+// Set route for OAuth redirect
+app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/fail' }));
+
+// test - not needed anymore
+app.get('/auth/twitch/test', passport.authenticate('twitch', { scope: 'analytics:read:games channel:read:subscriptions', successRedirect: '/test', failureRedirect: '/fail' }));
+
+app.get('/test', async function (req, res) {
+  if(req.session && req.session.passport && req.session.passport.user) {
+    try {
+      accessToken = req.session.passport.user.accessToken;
+      userId = req.session.passport.user.id;
+      const gameAnalytics = await controller.getAnalyticsGames(accessToken);
+      const bitsLeaderboard = await controller.getBitsLeaderboard(accessToken);
+      const polls = await controller.getPolls(accessToken, userId);
+
+      var choices = [{
+        'title':'Heads'
+      },
+      {
+        'title':'Tails'
+      }]
+      //const poll = await controller.createPoll(accessToken, userId, 'Heads or tails?', choices, 15);
+      const prediction = await controller.createPrediction(accessToken, userId, "Heads or tails?", choices, 30);
+      res.json([polls, gameAnalytics, bitsLeaderboard]);
+      //console.log(gameAnalytics);
+      //console.log(bitsLeaderboard);
+    } catch (error) {
+      console.error(error);
+    res.status(500).json({ message: 'An error occurred while fetching test' });
+    }
+  } else {
+    res.redirect('/');
+  }
+});
+
+// If user has an authenticated session, display it, otherwise display link to authenticate
+app.get('/fail', function (req, res) {
+    res.send('<html><head><title>Twitch Auth Sample</title></head> <p>FAILED</p></html>');
+});
+
+// Define a simple template to safely generate HTML with values from user's profile
+var template = handlebars.compile(`
+<html><head><title>Twitch Auth Sample</title></head>
+<table>
+    <tr><th>Access Token</th><td>yes</td></tr>
+    <tr><th>Refresh Token</th><td>yes</td></tr>
+    <tr><th>Display Name</th><td>{{display_name}}</td></tr>
+    <tr><th>Bio</th><td>{{bio}}</td></tr>
+    <tr><th>Image</th><td>{{logo}}</td></tr>
+    <tr><th>Test:</th><td>{{test}}</td></tr>
+    <tr><td><img src={{image_url}} alt="Dvd Profile Image"></td></tr>
+    <button onClick="location.href = '/test'">Click me!!!</button>
+</table></html>`);
+
+// If user has an authenticated session, display it, otherwise display link to authenticate
+app.get('/', function (req, res) {
+  if(req.session && req.session.passport && req.session.passport.user) {
+    res.send(template(req.session.passport.user));
+  } else {
+    res.send('<html><head><title>Twitch Auth Sample</title></head> <a href="/auth/twitch">test</a><br><a href="/auth/twitch/test">test2</a></html>');
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Twitch auth sample listening on port 3000!')
+});
---
+++ serverController.js
@@ -0,0 +1,93 @@
+// Define our dependencies
+const axios          = require('axios');
+const { noConflict } = require('handlebars');
+require('dotenv').config();
+
+// Define constants
+const TWITCH_CLIENT_ID = process.env.CLIENT_ID;
+const TWITCH_SECRET    = process.env.SECRET_KEY;
+
+// Function to get channel games analytics information
+async function getAnalyticsGames(accessToken) {
+    const response = await axios.get('https://api.twitch.tv/helix/analytics/games', {
+        headers: {
+        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': `Bearer ${accessToken}`
+        }
+    });
+return response.data.data;
+}
+  
+// Function to get bits leaderboard information
+async function getBitsLeaderboard(accessToken) {
+    const response = await axios.get('https://api.twitch.tv/helix/bits/leaderboard', {
+        headers: {
+        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': `Bearer ${accessToken}`
+        }
+    });
+return response.data;
+}
+  
+// Function to get polls information
+async function getPolls(accessToken, userId) {
+    const response = await axios.get(`https://api.twitch.tv/helix/polls`, {
+        headers: {
+        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': `Bearer ${accessToken}`
+        },
+        params: {
+            broadcaster_id: userId
+        },
+    });
+    return response.data;
+}
+
+// Function to create a poll
+// choices: [ {"title":"Heads"}, {"title":"tails"} ]
+async function createPoll(accessToken, userId, pollTitle, pollChoices, pollDuration=15) {
+    const response = await axios.post(`https://api.twitch.tv/helix/polls`, {
+            'broadcaster_id': userId,
+            'title': pollTitle,
+            'choices': pollChoices,
+            'duration': pollDuration
+        }, {
+        headers: {
+        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    return response.data;
+}
+
+/** 
+ * Function to create a prediction
+ * @accessToken {string} access token - requires channel:manage:predictions scope
+ * @userId {string} broadcaster user id
+ * @predictionTitle {string} max 45 chars
+ * @predictionOutcomes {list[ {"title":"outcome1"}, {"title":"outcome2"} ]} 2-10 outcomes, title max 25 characters
+ * @predictionWindow {integer} length of prediction window in seconds, default/minimum = 30, max = 1800 (30 mins)
+ */ 
+async function createPrediction(accessToken, userId, predictionTitle, predictionOutcomes, predictionWindow=30) {
+    const response = await axios.post(`https://api.twitch.tv/helix/predictions`, {
+            'broadcaster_id': userId,
+            'title': predictionTitle,
+            'outcomes': predictionOutcomes,
+            'prediction_window': predictionWindow
+        }, {
+        headers: {
+        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    return response.data;
+}
+
+
+module.exports = {
+    getAnalyticsGames,
+    getBitsLeaderboard,
+    getPolls,
+    createPoll,
+    createPrediction
+}
